0753a22fce4c61f727a2ddaa6e0a7ad76c6923ea
Andrew Stalick <andy@stalick.net>
2015-11-29 14:34:46 -0600

    Fix broken darwin url, now using tar.gz. Remove unused code in test. Bump bin-check version

--- test/test.js
+++ test/test.js
@@ -1,30 +1,13 @@
-/*global afterEach,beforeEach,it*/
 'use strict';
 
 var assert = require('assert');
-var execFile = require('child_process').execFile;
-var fs = require('fs');
-var path = require('path');
 var binCheck = require('bin-check');
-var BinBuild = require('bin-build');
-var compareSize = require('compare-size');
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
-var tmp = path.join(__dirname, 'tmp');
 
-beforeEach(function () {
-	mkdirp.sync(tmp);
-});
-
-afterEach(function () {
-	rimraf.sync(tmp);
-});
-
-it('return path to binary and verify that it is working', function (cb) {
-	binCheck(require('../'), ['-version'], function (err, works) {
-		assert(!err);
+it('returns path to binary and verifies that it is working', function (cb) {
+	binCheck(require('../'), ['--help']).then( function(works){
 		assert(works);
 		cb();
-	});
+	} )
 });
+
 
